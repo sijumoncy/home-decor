aebe6a380c01b6ab2c9b5380e0a72a2a70d60b1c
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-12-23 13:25:23 +0530

    added routes structures

--- server/app.js
+++ server/app.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const userRoute = require('./routes/v1/user')
+const authRoute = require('./routes/v1/auth')
+const cartRoute = require('./routes/v1/cart')
+const orderRoute = require('./routes/v1/order')
+const productRoute = require('./routes/v1/product')
 const config = require('./config/config')
 
 const app = express();
 
 
 app.use(`${config.apiBaseUrl}/user`, userRoute)
+app.use(`${config.apiBaseUrl}/auth`, authRoute)
+app.use(`${config.apiBaseUrl}/product`, productRoute)
+app.use(`${config.apiBaseUrl}/cart`, cartRoute)
+app.use(`${config.apiBaseUrl}/order`, orderRoute)
 
 
 module.exports = app;
--- server/routes/v1/auth.js
+++ server/routes/v1/auth.js
@@ -1,6 +1,6 @@
 const router = require('express').Router()
 
-
+// Registration
 
 
 module.exports = router
