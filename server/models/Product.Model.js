a63ba9f213966b5e0ad1ec5381ddb6c182ea4425
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-12-23 13:21:53 +0530

    creared basic schemas for user, products, order ,cart etc

---
+++ server/models/Product.Model.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const productSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    img: { type: String },
+    categories: { type: Array },
+    size: { type: String },
+    colors: { type: String },
+    price: { type: Number },
+    rating: { type: Number },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Product", productSchema);
