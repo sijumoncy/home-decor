5c2f01ea0d4838cbec0042dee6dac4f826dd0dde
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-12-27 16:54:48 +0530

    mob view for whyUs expand

--- client/components/whyUs/WhyUs.tsx
+++ client/components/whyUs/WhyUs.tsx
@@ -1,19 +1,47 @@
 import React from "react";
 import WhyUsCard from "./WhyUsCard";
-import { whyUsContents } from "@/constants/whyus";
+import { whyUsContents, whyUsOffers } from "@/constants/whyus";
+import Image from "next/image";
 
 function WhyUs() {
   return (
     <section className="whyus__container">
-      {whyUsContents.map((content, index) => (
-        <div key={content.id} className="card" style={{top: `calc(var(--navbar-offset) * ${(index +1 )})`}}>
-          <WhyUsCard
-            {...content}
-            imageUrl={`/Assets/whyus${content.id}.webp`}
-            bgCss={content.bgColor}
-          />
+      <div>
+        {whyUsContents.map((content, index) => (
+          <div
+            key={content.id}
+            className="card"
+            style={{ top: `calc(var(--navbar-offset) * ${index + 1})` }}
+          >
+            <WhyUsCard
+              {...content}
+              imageUrl={`/Assets/whyus${content.id}.webp`}
+              bgCss={content.bgColor}
+            />
+          </div>
+        ))}
+      </div>
+      <div className="features_section">
+        <p className="title">{whyUsOffers.title}</p>
+        <div className="feature-grid">
+          {whyUsOffers.offers.map((feature) => (
+            <div key={feature.id} className="feature-box">
+              <div className="feature-img">
+                <Image
+                  src={feature.img}
+                  alt="quality"
+                  className="icon"
+                  fill={true}
+                />
+              </div>
+              <div className="feature-content">
+                <p className="head">{feature.point}</p>
+                <p className="para">{feature.description}</p>
+              </div>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </section>
   );
 }
--- client/constants/whyus.ts
+++ client/constants/whyus.ts
@@ -5,10 +5,12 @@ export const whyUsContents = [
     stepAlpha: "one",
     head: "why choose us",
     tagline: "we translate your ideas into unique and functional environments",
-    title:"We transform your ideas into extraordinary spaces",
-    description:"For over years we have been translating customers' ideas into unique and functional environments, with professionalism and extreme attention to detail.",
-    description2:"We know that needs change throughout life, and our interior design, planning and furniture supply services allow you to adapt your spaces flexibly, ensuring they grow with you and always reflect your personality and lifestyle .",
-    bgColor : '#fff'
+    title: "We transform your ideas into extraordinary spaces",
+    description:
+      "For over years we have been translating customers' ideas into unique and functional environments, with professionalism and extreme attention to detail.",
+    description2:
+      "We know that needs change throughout life, and our interior design, planning and furniture supply services allow you to adapt your spaces flexibly, ensuring they grow with you and always reflect your personality and lifestyle .",
+    bgColor: "#fff",
   },
   {
     id: 2,
@@ -16,10 +18,12 @@ export const whyUsContents = [
     stepAlpha: "two",
     head: "why choose us",
     tagline: "for a unique result that will surprise you",
-    title:"We manage the project from start to finish",
-    description:"From the initial idea to implementation, we take care of your project at 360°. Thanks to our experience and expertise, your space will be created exactly as you have always dreamed of, without worries.",
-    description2:"We know that needs change throughout life, and our interior design, planning and furniture supply services allow you to adapt your spaces flexibly, ensuring they grow with you and always reflect your personality and lifestyle .",
-    bgColor : '#dbedff'
+    title: "We manage the project from start to finish",
+    description:
+      "From the initial idea to implementation, we take care of your project at 360°. Thanks to our experience and expertise, your space will be created exactly as you have always dreamed of, without worries.",
+    description2:
+      "We know that needs change throughout life, and our interior design, planning and furniture supply services allow you to adapt your spaces flexibly, ensuring they grow with you and always reflect your personality and lifestyle .",
+    bgColor: "#dbedff",
   },
   {
     id: 3,
@@ -27,10 +31,12 @@ export const whyUsContents = [
     stepAlpha: "three",
     head: "why choose us",
     tagline: "we guide you through everything from dream to reality",
-    title:"We create the spaces of your dreams to measure",
-    description:"We optimize space and offer creative solutions to make your environment a harmonious and impactful place. Through the renderings that we will prepare for you, you will be able to visualize the result already in the preliminary phase. Your project will come to life exactly as you imagined it.",
-    description2:"Our team of experts will take care of assembling the furniture with precision, effectively solving any problems on site. ",
-    bgColor : '#b6daff'
+    title: "We create the spaces of your dreams to measure",
+    description:
+      "We optimize space and offer creative solutions to make your environment a harmonious and impactful place. Through the renderings that we will prepare for you, you will be able to visualize the result already in the preliminary phase. Your project will come to life exactly as you imagined it.",
+    description2:
+      "Our team of experts will take care of assembling the furniture with precision, effectively solving any problems on site. ",
+    bgColor: "#b6daff",
   },
   {
     id: 4,
@@ -38,9 +44,39 @@ export const whyUsContents = [
     stepAlpha: "four",
     head: "why choose us",
     tagline: "We ensure implement dreams with quality and proffesiona; touch",
-    title : 'Quality and Proffessionalism',
-    description:'We firmly believe that the quality and proffessional implementation is the key element in creating confortable and satisfying spaces. We have selected suppliers, well qualified teams who bring highest quality and wide range of customization options.',
-    description2:"The idea for furnishing that are long-lasting, functional and confortable is our priority. Each piece of work we offer is not only a style statemenet , but piece of work with love and passion.",
-    bgColor : '#a4d1ff'
+    title: "Quality and Proffessionalism",
+    description:
+      "We firmly believe that the quality and proffessional implementation is the key element in creating confortable and satisfying spaces. We have selected suppliers, well qualified teams who bring highest quality and wide range of customization options.",
+    description2:
+      "The idea for furnishing that are long-lasting, functional and confortable is our priority. Each piece of work we offer is not only a style statemenet , but piece of work with love and passion.",
+    bgColor: "#a4d1ff",
   },
 ];
+
+export const whyUsOffers = {
+  title:
+    "We design unique, elegant and tailor-made environments: spaces that tell your story",
+  offers: [
+    {
+      id: 1,
+      point: "Experience",
+      description:
+        "Many years of experience with proffessional talents creates perfect space for you dreams",
+      img: "/Assets/rating.svg",
+    },
+    {
+      id: 2,
+      point: "Quality",
+      description:
+        "We always focus on quality by choose the best accessories to guarantee beauty, functionality and durability over years",
+      img: "/Assets/focus.svg",
+    },
+    {
+      id: 3,
+      point: "Details",
+      description:
+        "Attention to detail is our key. Our team will look closely into the cases and tailor everyhting make you feel at home",
+      img: "/Assets/quality.svg",
+    },
+  ],
+};
