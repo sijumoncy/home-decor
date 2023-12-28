537272891bb7e827b953ffb0a27b9893249ebab2
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-12-28 17:58:21 +0530

    done login page simple design for testing

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,8 +1,40 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div className="login__container">
+      <h4>Login</h4>
+      <form>
+        <input
+          className="username"
+          name="username"
+          id="username"
+          type="text"
+          placeholder="username"
+          required
+        />
+        <input
+          className="password"
+          name="password"
+          id="password"
+          type="password"
+          placeholder="password"
+          required
+        />
+
+        <button className="login-btn">Login</button>
+      </form>
+
+        <div className="noaccount-acc">
+            <span>Not Signed Up Yet? </span>
+            &nbsp;&nbsp;
+            <Link href="/signup">SignUp</Link>
+            &nbsp;&nbsp;
+            <span>Now</span>
+        </div>
+
+    </div>
   )
 }
 
