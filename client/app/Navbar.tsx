5d0b2fcb608d56bec588b2a117bb2ed342de90ce
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-12-29 18:26:39 +0530

    added active change for login and register routes

--- client/app/Navbar.tsx
+++ client/app/Navbar.tsx
@@ -1,38 +1,38 @@
-"use client"
+"use client";
 
 import MobNavIcon from "@/components/navbar/MobNavIcon";
 import NavMenu from "@/components/navbar/NavMenu";
+import useAppContext from "@/context/appContext";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { FaRegUser, FaOpencart } from "react-icons/fa";
 
-
-
 function Navbar() {
+  const [sticky, setSticky] = useState(false);
 
-  const [sticky, setSticky] = useState(false)
-
-  const handleScroll = (event:Event) => {
+  const handleScroll = (event: Event) => {
     const currentScollY = event.currentTarget?.scrollY;
-    if(currentScollY > 50) {
-      setSticky(true)
-    } else{
-      setSticky(false)
+    if (currentScollY > 50) {
+      setSticky(true);
+    } else {
+      setSticky(false);
     }
-  }
-  
+  };
 
-  useEffect(() =>{
-    window.addEventListener('scroll', handleScroll)
+  const { currentMenu, setCurrentMenu } = useAppContext();
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
 
     return () => {
-      window.removeEventListener('scroll', handleScroll)
-    }
-  },[])
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
-    <nav className={`nav__container ${sticky && 'sticky'}`}>
+    <nav className={`nav__container ${sticky && "sticky"}`}>
       <div className="wrapper">
         <div className="logo">
           <Image
@@ -45,7 +45,15 @@ function Navbar() {
         </div>
         <div className="icons">
           <CiSearch style={{ strokeWidth: "2" }} className="icon" />
-          <FaRegUser className="icon" />
+          <Link
+            href="/login"
+            className={`icon-link ${
+              currentMenu === "login/register" && "active"
+            }`}
+            onClick={() => setCurrentMenu("login/register")}
+          >
+            <FaRegUser className="icon" />
+          </Link>
           <FaOpencart className="icon" />
           <MobNavIcon />
         </div>
