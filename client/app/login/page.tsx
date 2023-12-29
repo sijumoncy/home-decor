ea17ba271081b97f6c3380b1dde732432cac965b
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2023-12-29 18:46:49 +0530

    added loading and form handling in login page

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -1,11 +1,41 @@
-import Link from 'next/link'
-import React from 'react'
+"use client";
+import LoaderLine from "@/components/utils/Loader/LoaderLine";
+import Link from "next/link";
+import React, { FormEvent, useState } from "react";
 
 function Login() {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target as {
+      name: "password" | "username";
+      value: string;
+    };
+
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    setLoading(true);
+    console.log("form event target : ", formData);
+    setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+  };
+
   return (
     <div className="login__container">
       <h4>Login</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           className="username"
           name="username"
@@ -13,6 +43,8 @@ function Login() {
           type="text"
           placeholder="username"
           required
+          value={formData.username}
+          onChange={handleChange}
         />
         <input
           className="password"
@@ -21,21 +53,26 @@ function Login() {
           type="password"
           placeholder="password"
           required
+          value={formData.password}
+          onChange={handleChange}
         />
 
-        <button className="login-btn">Login</button>
+        {loading ? (
+          <LoaderLine text="" primaryColor="#808080" secondaryColor="#adaaaa" />
+        ) : (
+          <button className="login-btn">Login</button>
+        )}
       </form>
 
-        <div className="noaccount-acc">
-            <span>Not Signed Up Yet? </span>
-            &nbsp;&nbsp;
-            <Link href="/signup">SignUp</Link>
-            &nbsp;&nbsp;
-            <span>Now</span>
-        </div>
-
+      <div className="noaccount-acc">
+        <span>Not Signed Up Yet? </span>
+        &nbsp;&nbsp;
+        <Link href="/signup">SignUp</Link>
+        &nbsp;&nbsp;
+        <span>Now</span>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
