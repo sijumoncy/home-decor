1d77ffbc93393ab0f382e0549e8f8bf6b5fa4257
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-01 19:43:12 +0530

    basic layout and buttons added for porduct managmement

--- client/components/Admin/product/ManageProduct.tsx
+++ client/components/Admin/product/ManageProduct.tsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { FaPlus } from "react-icons/fa";
 
 function ManageProduct() {
   return (
-    <div>ManageProduct</div>
-  )
+    <section className="product__container">
+      <div className="header-section">
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-inp"
+            placeholder="search products"
+          />
+          <CiSearch style={{ strokeWidth: "2" }} className="icon-search" />
+        </div>
+
+        <button className="add-btn">
+          Add <FaPlus className="icon-plus" />
+        </button>
+      </div>
+
+      <div className="content-section"></div>
+    </section>
+  );
 }
 
-export default ManageProduct
+export default ManageProduct;
