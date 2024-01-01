c8891a44b31681f204a7dcdebf40a0b4e116f473
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-01 19:14:05 +0530

    added dynamic component rendering for admin menus

--- client/app/(admin)/admin/page.tsx
+++ client/app/(admin)/admin/page.tsx
@@ -1,13 +1,24 @@
 "use client";
+import ManageCustomer from "@/components/Admin/customer/ManageCustomer";
+import AdminDashboard from "@/components/Admin/dashboard/AdminDashboard";
+import ManageOrder from "@/components/Admin/order/ManageOrder";
+import ManageProduct from "@/components/Admin/product/ManageProduct";
 import useAdminContext from "@/context/adminContext";
 import React from "react";
 
+const Components:{[key:string] : () => React.JSX.Element} = {
+  product : ManageProduct,
+  customer : ManageCustomer,
+  order : ManageOrder,
+  dashboard : AdminDashboard
+}
+
 function AdminHome() {
   const { currentMenu } = useAdminContext();
-
-  console.log({ currentMenu });
-
-  return <div>AdminHome : {currentMenu}</div>;
+  console.log({currentMenu});
+  
+  const Component = Components[currentMenu.toLowerCase()]
+  return currentMenu ? <Component/> : <></>
 }
 
 export default AdminHome;
---
+++ client/components/Admin/customer/ManageCustomer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ManageCustomer() {
+  return (
+    <div>ManageCustomer</div>
+  )
+}
+
+export default ManageCustomer
