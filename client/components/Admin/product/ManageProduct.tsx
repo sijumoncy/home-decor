a20cee4d4fdd6506660817333781a427998ad02e
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-01 21:35:41 +0530

    added modal component

--- client/components/Admin/product/ManageProduct.tsx
+++ client/components/Admin/product/ManageProduct.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+"use client";
+
+import Modal from "@/components/modal/Modal";
+import React, { useState } from "react";
 import { CiSearch } from "react-icons/ci";
 import { FaPlus } from "react-icons/fa";
 
 function ManageProduct() {
+  const [isModalOpen, setModalOpen] = useState(false);
+
+  const handleOpenModal = () => {
+    setModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setModalOpen(false);
+  };
+
+  const handleOnModalAction = () => {
+    handleCloseModal();
+    console.log("action modal clicked");
+    
+  };
+
   return (
     <section className="product__container">
       <div className="header-section">
@@ -15,12 +34,22 @@ function ManageProduct() {
           <CiSearch style={{ strokeWidth: "2" }} className="icon-search" />
         </div>
 
-        <button className="add-btn">
+        <button className="add-btn" onClick={handleOpenModal}>
           Add <FaPlus className="icon-plus" />
         </button>
       </div>
 
       <div className="content-section"></div>
+
+      <Modal
+        title="Create New Product"
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        onAction={handleOnModalAction}
+        actionBtnName="Create"
+      >
+        <p>This is the content of the modal.</p>
+      </Modal>
     </section>
   );
 }
---
+++ client/components/modal/Modal.tsx
@@ -0,0 +1,40 @@
+import React, { ReactNode } from 'react';
+
+interface ModalProps {
+  title: string;
+  isOpen: boolean;
+  onClose: () => void;
+  children: ReactNode;
+  onAction?: () => void;
+  actionBtnName?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({ title, isOpen, onClose, children, onAction, actionBtnName = "process" }) => {
+  const modalClassName = isOpen ? 'modal-container open' : 'modal-container';
+
+  return (
+    <div className={modalClassName}>
+      <div className="modal">
+        <div className="modal-header">
+          <h2>{title}</h2>
+          <button className="close-button" onClick={onClose}>
+            &times;
+          </button>
+        </div>
+        <div className="modal-body">{children}</div>
+        <div className="modal-footer">
+          <button className="cancel-button" onClick={onClose}>
+            Cancel
+          </button>
+          {onAction && (
+            <button className="save-button" onClick={onAction}>
+              {actionBtnName}
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
