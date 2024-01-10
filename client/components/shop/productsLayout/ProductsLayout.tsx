9befd0ef2a3f73e2cda867755c7d9a8c03755ea0
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-10 21:36:27 +0530

    fetch categorised products to show

--- client/components/shop/productsLayout/ProductsLayout.tsx
+++ client/components/shop/productsLayout/ProductsLayout.tsx
@@ -6,13 +6,14 @@ import React, { useEffect, useState } from "react";
 
 function ProductsLayout() {
   const [trending, setTrending] = useState<IProductResponse[]>([]);
+  const [products, setProducts] = useState<{[key:string] : IProductResponse[]}>({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
-  const getTrendingProduct = async () => {
+  const getProduct = async () => {
     setLoading(true);
-    const { data, error } = await getProductsService(0, 15, "");
-    console.log(data.products[0]);
+    const { data, error } = await getProductsService(0, 30, "");
+    console.log(data.products);
     if (!error) {
       setError(false);
       setTrending(data.products);
@@ -23,8 +24,29 @@ function ProductsLayout() {
     }
   };
 
+  const getCategoriesProducts = async () => {
+
+    const { data, error } = await getProductsService(0, 50, "", {
+      category:["furniture", "lighting", "smarthome", "accessories", "modern"]
+    });
+    const {products}:{products:IProductResponse[]} = data
+    const funitures = products.filter((product) => product.categories.includes('furniture'))
+    const lighting = products.filter((product) => product.categories.includes('lighting'))
+    const smarthome = products.filter((product) => product.categories.includes('smarthome'))
+    const accessories = products.filter((product) => product.categories.includes('accessories'))
+
+    if(!error) {
+      setProducts({funitures, lighting, smarthome, accessories})
+    }
+
+  }
+
+  console.log(products);
+  
+
   useEffect(() => {
-    getTrendingProduct();
+    getProduct();
+    getCategoriesProducts()
   }, []);
 
   return (
@@ -35,9 +57,10 @@ function ProductsLayout() {
         <>loading..</>
       ) : (
         <>
-          <div className="two-col">
+          <div className="product-sections">
+
             {/* section 1 */}
-            <div className="two-one">
+            <div className="section-one">
               <div className="two-one-card">
                 <div className="head">COMBO</div>
                 <div className="img">
@@ -59,7 +82,7 @@ function ProductsLayout() {
             </div>
 
             {/* section 2 */}
-            <div className="two-two">
+            <div className="section-two">
               {/* cards */}
               {[3, 7, 8, 6].map((num, index) => (
                 <div key={num} className="two-cards">
@@ -83,6 +106,14 @@ function ProductsLayout() {
                 </div>
               ))}
             </div>
+
+            {/* section3 */}
+            <div className="section-three">
+              <div className="">
+
+              </div>
+            </div>
+
           </div>
         </>
       )}
