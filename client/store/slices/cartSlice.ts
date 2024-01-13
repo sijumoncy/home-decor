877d1b8e72ed02dec6005373b7f429aa1404487d
sijumoncy <72241997+sijumoncy@users.noreply.github.com>
2024-01-13 10:59:40 +0530

    created the inc and dec fucntion along with card reducer

--- client/store/slices/cartSlice.ts
+++ client/store/slices/cartSlice.ts
@@ -14,17 +14,34 @@ export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-
-    increment : (state, action:PayloadAction<IProductResponse>) => {
-      const item = state.cartItems.find((el) => el.product._id === action.payload._id )
-      if(item) item.quantity ++;
+    increment: (state, action: PayloadAction<IProductResponse>) => {
+      const item = state.cartItems.find(
+        (el) => el.product._id === action.payload._id
+      );
+      if (item) item.quantity++;
       else {
         state.cartItems.push({
-          product : action.payload,
-          quantity : 1
-        })
+          product: action.payload,
+          quantity: 1,
+        });
       }
-    } 
+    },
 
+    decrement: (state, action: PayloadAction<IProductResponse>) => {
+      const item = state.cartItems.find(
+        (el) => el.product._id === action.payload._id
+      );
+      if (item) {
+        item.quantity--;
+        if (item.quantity === 0) {
+          state.cartItems = state.cartItems.filter(
+            (el) => el.product._id !== action.payload._id
+          );
+        }
+      }
+    },
   },
 });
+
+export const { increment, decrement } = cartSlice.actions;
+export default cartSlice.reducer;
